4b9b2b48be77b8c9641c2acd5033073044e21349
Navil <mdnavil922@gmail.com>
2023-09-24 18:08:06 +0530

    Third Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Student", "Working"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
